bf13378ad3910c1daac54a02cb11bd9323d8ad79
Isadoradodora <00001141091318sp@al.educacao.sp.gov.br>
2024-11-18 21:53:21 -0300

    Add files via upload

---
+++ ferramenta de estudo Isadora/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Portugues',
+    'O que é adjetivo?',
+    'palavra que se junta ao substantivo para modificar o seu significado, acrescentando-lhe noções de qualidade, natureza, estado etc.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Minas Gerais?',
+    'A capital de Minas Gerais é Belo Horizonte'
+)
+
+criaCartao(
+    'Fisica',
+    'O que é H2o?',
+    'é um líquido formado por dois átomos de hidrogênio e um de oxigênio.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz casa em Inglês?',
+    'Casa em ingles é House'
+)
